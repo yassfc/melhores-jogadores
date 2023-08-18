2e4e9cce019cac84d01ead89f65b642cae3ea54a
yassfc <141974926+yassfc@users.noreply.github.com>
2023-08-18 20:39:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+
+public class Jogador{
+
+      public int energia;
+      public bool situacao;
+      public string nome;
+             public Jogador(string n){
+        energia = 10;
+        situacao = true;
+        nome = n;
+
+      }
+
+   }
+
+      class teste{
+        static void Main (){
+
+          Jogador j1=new Jogador("Messi");
+          Jogador j2=new Jogador("Neymar");
+          Jogador j3=new Jogador("Cristiano Ronaldo");
+
+           Console.WriteLine("Nome do melhor jogador do mundo é {0}", j1.nome);
+
+           Console.WriteLine("    ");
+
+           Console.WriteLine("Nome do segundo melhor jogador do mundo é {0}", j3.nome);
+
+           Console.WriteLine("    ");
+
+            Console.WriteLine("Nome do terceiro melhor jogador do mundo é {0}", j2.nome);
+
+            Console.ReadLine();
+            Console.Clear();
+
+
+
+
+
+
+        }
+
+
+      }
+    
+    
+
+
+
+    
+
